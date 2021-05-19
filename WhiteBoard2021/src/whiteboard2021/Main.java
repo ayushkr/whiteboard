fb98879a41ee197002f081c0db42a3491141978d
Ayush K R <emailidofayush@gmail.com>
2021-05-19 18:52:33 +0530

    ss

--- WhiteBoard2021/src/whiteboard2021/Main.java
+++ WhiteBoard2021/src/whiteboard2021/Main.java
@@ -15,7 +15,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-       // hello changed in github
+       // hello changed in github 2
     }
     
 }
